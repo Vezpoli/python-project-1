240eb1c344edcd4ae54081fbb94131c6ab988fd0
Vezpoli <94568002+Vezpoli@users.noreply.github.com>
2025-03-10 11:25:14 -0400

    Update port_scanner.py

--- port_scanner.py
+++ port_scanner.py
@@ -1,7 +1,7 @@
 import nmap
 
 scanner = nmap.PortScanner()
-target_ip = "192.168.1.1"  # Change this to your IP
+target_ip = " 107.175.102.154"  # Change this to your IP
 
 print(f"🔍 Scanning target: {target_ip}...")
 
